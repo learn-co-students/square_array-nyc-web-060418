bf17539f72f7dc8b43c19c343c1ba7d431d1c375
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 07:51:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
   # your code here
+  result = [] 
+  array.each do |i|
+    result.push(i ** 2)
+  end 
+  result 
 end
+
+=begin 
+def square_array(array)
+  result = array.collect{|i| i ** 2}
+  result 
+end 
+=end 
+ 
+  
