70b9bf002af72f76b7d512a5f719a047b412b0e2
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-05-26 14:19:29 -0400

    implemented method with each, and with collect

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  returnArray = []
+  array.each do |number|
+    returnArray.push(number ** 2)
+  end
+  returnArray
+end
+
+def collect_array(array)
+  array.collect {|number| number ** 2 }
 end
